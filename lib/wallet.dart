afa791da895d495c7945060a976ad4274210f417
Jason Hancock <dhancock@figure.com>
2022-01-25 14:54:13 -0600

    updated wallet exports

--- lib/wallet.dart
+++ lib/wallet.dart
@@ -4,3 +4,5 @@ export 'src/wallet/coin.dart';
 export 'src/wallet/private_key.dart';
 export 'src/wallet/public_key.dart';
 export 'src/wallet/mnemonic.dart';
+export 'src/wallet/crypto/encryption/crypto.dart';
+export 'src/wallet/crypto/hash/hash.dart';
