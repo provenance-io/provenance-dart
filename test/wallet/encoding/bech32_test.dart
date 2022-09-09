9a74406ae1390941ed2a215597c1bef91f735794
Jason Hancock <dhancock@figure.com>
2022-09-09 12:08:30 -0500

    fixed: bech32 decode and updated tests

--- test/wallet/encoding/bech32_test.dart
+++ test/wallet/encoding/bech32_test.dart
@@ -1,7 +1,96 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provenance_dart/src/wallet/encoding/bech32.dart';
 
+const encodedData1 = [
+  0x00,
+  0x01,
+  0x02,
+  0x03,
+  0x04,
+  0x05,
+  0x06,
+  0x07,
+  0x08,
+  0x09,
+  0xa1,
+  0xa2,
+  0xa3,
+  0xa4,
+  0xa5,
+  0xa6,
+  0xa7,
+  0xa8,
+  0xa9,
+  0xb0
+];
+
+const encodedData2 = [
+  0xFF,
+  0xFE,
+  0xFD,
+  0xFC,
+  0xFB,
+  0xFA,
+  0xF9,
+  0xF8,
+  0xF7,
+  0xF6,
+  0xF5,
+  0xF4,
+  0xF3,
+  0xF2,
+  0xF1,
+  0xF0
+];
+
+class _testCase {
+  final String hrp;
+  final String bech32String;
+  final List<int> encodedData;
+
+  const _testCase(
+      {required this.hrp,
+      required this.bech32String,
+      required this.encodedData});
+}
+
+const _testDataSet = [
+  _testCase(
+      hrp: "tp",
+      bech32String: "tp1qqqsyqcyq5rqwzqf5x328f9956n632ds27t4tf",
+      encodedData: encodedData1),
+  _testCase(
+      hrp: "test%=",
+      bech32String: "test%=1qqqsyqcyq5rqwzqf5x328f9956n632ds49wcws",
+      encodedData: encodedData1),
+  _testCase(
+      hrp: "tp",
+      bech32String: "tp1lll0ml8mltul3alk7h608uh37qtz7avc",
+      encodedData: encodedData2),
+  _testCase(
+      hrp: "test%=",
+      bech32String: "test%=1lll0ml8mltul3alk7h608uh37qnlyj4e",
+      encodedData: encodedData2)
+];
+
 main() {
+  test("encode", () {
+    for (final testData in _testDataSet) {
+      final decoded = Bech32().encode(testData.hrp, testData.encodedData, "1");
+
+      expect(decoded, testData.bech32String);
+    }
+  });
+
+  test("decode", () {
+    for (final testData in _testDataSet) {
+      final decoded = Bech32().decode(testData.bech32String)!;
+
+      expect(decoded.fiveBitData, testData.encodedData);
+      expect(decoded.hrp, testData.hrp);
+    }
+  });
+
   test('valid', () {
     const strings = [
       "A12UEL5L",
