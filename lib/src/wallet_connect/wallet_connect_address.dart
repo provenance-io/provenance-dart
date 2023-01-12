195140332076a192f5f98940daa2825a7d1ef5d6
Jason Hancock <dhancock@figure.com>
2023-01-12 15:44:58 -0600

    updated wallet connect address to support the parameters being in a different order. toString now returns the original string added support for optional parameters

--- lib/src/wallet_connect/wallet_connect_address.dart
+++ lib/src/wallet_connect/wallet_connect_address.dart
@@ -3,36 +3,95 @@ class WalletConnectAddress {
   final int version;
   final Uri bridge;
   final String key;
-  final String raw;
+  late String raw;
+  late Map<String, String> parameters;
 
   WalletConnectAddress._(
     this.topic,
     this.version,
     this.bridge,
     this.key,
-    this.raw,
-  );
+    String? raw,
+    Map<String, String> parameters,
+  ) {
+    this.parameters = Map.unmodifiable(parameters);
+
+    if (raw != null) {
+      this.raw = raw;
+    } else {
+      String paramStrings = "";
+      if (parameters.isNotEmpty) {
+        paramStrings =
+            parameters.entries.map((e) => "${e.key}=${e.value}").join("&");
+      }
+
+      this.raw =
+          "wc:$topic@$version?bridge=${Uri.encodeComponent(bridge.toString())}&key=$key${(paramStrings.isNotEmpty) ? "&$paramStrings" : ""}";
+    }
+  }
+
+  WalletConnectAddress(
+    String topic,
+    int version,
+    Uri bridge,
+    String key, [
+    Map<String, String>? parameters,
+  ]) : this._(
+            topic,
+            version,
+            bridge,
+            key,
+            "wc:$topic@$version?bridge=${Uri.encodeComponent(bridge.toString())}&key=$key${(parameters?.isNotEmpty ?? false) ? "&${parameters!.entries.map((e) => "${e.key}=${e.value}").join("&")}" : ""}",
+            parameters ?? <String, String>{});
 
   static WalletConnectAddress? create(String str) {
-    final regex = RegExp(
-        '^wc:(?<topic>[^@].+)@(?<version>\\d+)\\?bridge=(?<bridge>[^&]+)&key=(?<key>.+)\$');
+    final regex =
+        RegExp('^wc:(?<topic>[^@].+)@(?<version>\\d+)\\?(?<parameters>.*)\$');
     final match = regex.firstMatch(str);
 
     if (match == null) {
       return null;
     }
 
+    Uri? bridgeUri;
+    String? key;
+
+    final params = (match.namedGroup("parameters") ?? "").split("&");
+    final otherParameters = <String, String>{};
+
+    for (final param in params) {
+      final pieces = param.split("=");
+      if (pieces.length == 2) {
+        switch (pieces[0]) {
+          case "bridge":
+            bridgeUri = Uri.parse(Uri.decodeFull(pieces[1]));
+            break;
+          case "key":
+            key = pieces[1];
+            break;
+          default:
+            otherParameters[pieces[0]] = pieces[1];
+        }
+      }
+    }
+
+    if (bridgeUri == null || key == null) {
+      return null;
+    }
+
     final topic = match.namedGroup("topic")!;
     final version = int.parse(match.namedGroup("version")!);
-    final bridge = Uri.decodeFull(match.namedGroup("bridge")!);
-    final key = match.namedGroup("key")!;
 
     return WalletConnectAddress._(
       topic,
       version,
-      Uri.parse(bridge),
+      bridgeUri,
       key,
       str,
+      otherParameters,
     );
   }
+
+  @override
+  String toString() => raw;
 }
--- test/wallet_connect/wallet_connect_address_test.dart
+++ test/wallet_connect/wallet_connect_address_test.dart
@@ -1,20 +1,84 @@
-import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
 import 'package:provenance_dart/wallet_connect.dart';
 
-@GenerateMocks([WebSocket])
 main() {
   test("validAddress ", () {
-    const address =
-        "wc:TheTopic@12?bridge=http%3A%2F%2Ftester.com&key=MyTestKey";
-    final connectAddress = WalletConnectAddress.create(address);
+    const topic = "TheTopic";
+    const key = "MyTestKey";
+    final bridge = Uri.parse("http://tester.com");
+    final encodedBridge = Uri.encodeFull(bridge.toString());
+    const version = 12;
 
-    expect(connectAddress!.topic, "TheTopic");
-    expect(connectAddress.version, 12);
-    expect(connectAddress.bridge, Uri.parse("http://tester.com"));
-    expect(connectAddress.key, "MyTestKey");
+    {
+      final address = "wc:$topic@$version?bridge=$encodedBridge&key=$key";
+      final connectAddress = WalletConnectAddress.create(address);
+
+      expect(connectAddress!.topic, topic);
+      expect(connectAddress.version, version);
+      expect(connectAddress.bridge, bridge);
+      expect(connectAddress.key, key);
+      expect(connectAddress.raw, address);
+    }
+    // reorder params
+    {
+      final address = "wc:$topic@$version?key=$key&bridge=$encodedBridge";
+
+      final connectAddress = WalletConnectAddress.create(address);
+
+      expect(connectAddress!.topic, topic);
+      expect(connectAddress.version, version);
+      expect(connectAddress.bridge, bridge);
+      expect(connectAddress.key, key);
+      expect(connectAddress.raw, address);
+    }
+    // url is successfully processed even if there are extra parameters
+    {
+      final address =
+          "wc:$topic@$version?key=$key&bridge=$encodedBridge&extra1=444&extra2=100";
+
+      final connectAddress = WalletConnectAddress.create(address);
+
+      expect(connectAddress!.topic, topic);
+      expect(connectAddress.version, version);
+      expect(connectAddress.bridge, bridge);
+      expect(connectAddress.key, key);
+      expect(connectAddress.raw, address);
+    }
+  });
+
+  test("optional parameters are correctly extracted ", () {
+    const topic = "TheTopic";
+    const key = "MyTestKey";
+    final bridge = Uri.parse("http://tester.com");
+    final encodedBridge = Uri.encodeFull(bridge.toString());
+    const version = 12;
+    const otherParam1 = "AB";
+    const otherParam2 = "CD";
+
+    {
+      final address =
+          "wc:$topic@$version?bridge=$encodedBridge&key=$key&param1=$otherParam1&param2=$otherParam2";
+      final connectAddress = WalletConnectAddress.create(address);
+
+      expect(connectAddress!.parameters,
+          <String, String>{"param1": otherParam1, "param2": otherParam2});
+    }
+    {
+      final address =
+          "wc:$topic@$version?param1=$otherParam1&bridge=$encodedBridge&key=$key&param2=$otherParam2";
+      final connectAddress = WalletConnectAddress.create(address);
+
+      expect(connectAddress!.parameters,
+          <String, String>{"param1": otherParam1, "param2": otherParam2});
+    }
+    {
+      final address =
+          "wc:$topic@$version?bridge=$encodedBridge&param2=$otherParam2&key=$key&param1=$otherParam1";
+
+      final connectAddress = WalletConnectAddress.create(address);
+      expect(connectAddress!.parameters,
+          <String, String>{"param1": otherParam1, "param2": otherParam2});
+    }
   });
 
   test("returns null on invalid address ", () {
@@ -30,5 +94,17 @@ main() {
     expect(WalletConnectAddress.create(missingVersion), null);
     expect(WalletConnectAddress.create(decimalVersion), null);
     expect(WalletConnectAddress.create(missinKey), null);
+  });
+
+  test("toString returns an encoded address ", () {
+    final address = WalletConnectAddress(
+        "E75F695D-389F-4649-A25B-22FC458B6089",
+        2,
+        Uri.parse("http://www.test.com?param=1"),
+        "A9930B20-30E7-40DB-872C-F60BC33598E1",
+        <String, String>{"a": "Tester", "B": "123"});
+
+    expect(address.toString(),
+        "wc:E75F695D-389F-4649-A25B-22FC458B6089@2?bridge=http%3A%2F%2Fwww.test.com%3Fparam%3D1&key=A9930B20-30E7-40DB-872C-F60BC33598E1&a=Tester&B=123");
   });
 }
