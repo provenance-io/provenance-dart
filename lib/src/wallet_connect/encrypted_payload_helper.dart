ad2de8ed01e9203b92cf508ee510243bf0a7e2a3
Jacob Graves <88213669+jakegraves-figure@users.noreply.github.com>
2023-01-13 12:32:53 -0700

    Flutter keeps giving me an error here saying it's not encodable.

--- lib/src/wallet_connect/encrypted_payload_helper.dart
+++ lib/src/wallet_connect/encrypted_payload_helper.dart
@@ -32,7 +32,7 @@ class EncryptedPayloadHelper {
   EncryptionPayload encrypt(JsonEncodable encodable) {
     final ivBytes = Uint8List.fromList(
         List<int>.generate(16, (_) => math.Random().nextInt(255)));
-    final jsonString = jsonEncode(encodable);
+    final jsonString = jsonEncode(encodable.toJson());
     final responseBytes = utf8.encode(jsonString);
     final iv = IV(ivBytes);
 
