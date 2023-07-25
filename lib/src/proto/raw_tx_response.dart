34a9e27de7d6256f5e3c885f0477696dc07f3267
Jacob Graves <88213669+jakegraves-figure@users.noreply.github.com>
2023-07-25 12:17:38 -0600

    Adds provenance types.
    This addition prevents potential errors on consuming apps.

--- lib/src/proto/raw_tx_response.dart
+++ lib/src/proto/raw_tx_response.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:provenance_dart/src/proto/proto_gen/cosmos/base/abci/v1beta1/abci.pb.dart';
 import 'package:provenance_dart/src/proto/proto_gen/cosmos/tx/v1beta1/tx.pb.dart';
+import 'package:provenance_dart/type_registry.dart';
 
 class RawTxResponsePair {
   RawTxResponsePair(this.txRaw, this.txResponse);
@@ -11,8 +12,8 @@ class RawTxResponsePair {
 
   String asJsonString() {
     return """{
-     "txRaw": ${jsonEncode(txRaw.toProto3Json())}, 
-     "txResponse": ${jsonEncode(txResponse.toProto3Json())}
+     "txRaw": ${jsonEncode(txRaw.toProto3Json(typeRegistry: provenanceTypes))}, 
+     "txResponse": ${jsonEncode(txResponse.toProto3Json(typeRegistry: provenanceTypes))}
     }""";
   }
 }
